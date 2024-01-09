d3ff9c6205e4e6e3427835f6552e0c225302c873
Anurag Samarth <asamarth017@gmail.com>
2024-01-09 17:45:37 +0530

    Handle Input in React

--- ch1/src/components/ExpenseItem.jsx
+++ ch1/src/components/ExpenseItem.jsx
@@ -1,18 +1,33 @@
+import React, { useState } from "react";
+
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 
 const ExpenseItem = (props) => {
+
+  const [newTitle, setNewTitle] = useState("");
+    
   
+    const [title, setTitle] = useState(props.title);
+
+    const clickHandler = ()=>{
+      setTitle(newTitle);
+    }
+
+    const changeHandler = (event)=>{
+      setNewTitle(event.target.value);
+    }
+
   return (
     <div className="expense-item">
-      
        <ExpenseDate date={props.date}/>
-      
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <input type="text" value={newTitle} onChange={changeHandler} />
+      <button onClick={clickHandler}>Change Title</button>
     </div>
   );
 };
